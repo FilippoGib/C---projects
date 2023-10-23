d30990a7ced4152f0c38bb36b56179abb2a7a3f9
FilippoGib <105211605+FilippoGib@users.noreply.github.com>
2023-10-23 15:03:07 +0200

    commit 23/10/2023

--- cpp_functions/ComputeDefaultTax.cpp
+++ cpp_functions/ComputeDefaultTax.cpp
@@ -9,14 +9,19 @@ double computeTax(double subtotal,double taxRate = SALES_TAX);
 int main(int argc, char*argv[]){
 
     double subtotal;
+    double newTax;
 
     cout << "What is your bill? ";
-
     cin >> subtotal;
 
-    double total = subtotal + computeTax(subtotal);
+    cout << "That is the subtax? ";
+    cin >> newTax;
 
+    double total = subtotal + computeTax(subtotal);
+    double total2 = subtotal + computeTax(subtotal, newTax);
+    
     cout << "The total will be: " << total << endl ;
+    cout << "The second total will be " << total2 << endl ;
 }
 
 double computeTax(double subtotal,double taxRate /* se ho inserito un valore di default nella dichiarazione iniziale non posso riportarlo di nuovo! */){ 
